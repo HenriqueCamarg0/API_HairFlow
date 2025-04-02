dd055487e4ce25245f089de4c08972f239d8027a
Henrique <henry2camargo@gmail.com>
2025-04-01 22:07:21 -0300

    first commit

---
+++ src/main/java/com/HenriqueCamarg0/API_HairFlow/ApiHairFlowApplication.java
@@ -0,0 +1,13 @@
+package com.HenriqueCamarg0.API_HairFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHairFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHairFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HenriqueCamarg0/API_HairFlow/ApiHairFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HenriqueCamarg0.API_HairFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHairFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
