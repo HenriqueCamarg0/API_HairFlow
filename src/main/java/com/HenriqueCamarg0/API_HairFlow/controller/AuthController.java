f4412e2dd20dff6f7d1cee0adf37d1cf0b7150c0
Henrique <henry2camargo@gmail.com>
2025-04-17 21:58:47 -0300

    Refactor AuthController and UserService: remove unused User authentication logic from AuthController and ensure proper formatting in UserService

--- src/main/java/com/HenriqueCamarg0/API_HairFlow/controller/AuthController.java
+++ src/main/java/com/HenriqueCamarg0/API_HairFlow/controller/AuthController.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import lombok.RequiredArgsConstructor;
-
-import com.HenriqueCamarg0.API_HairFlow.Entity.User;
 import com.HenriqueCamarg0.API_HairFlow.dto.UserDTO;
 import com.HenriqueCamarg0.API_HairFlow.service.UserService;
 
@@ -35,7 +31,6 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDTO dto) {
         try {
-            User user = userService.authenticate(dto);
             return ResponseEntity.ok("Login realizado com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
